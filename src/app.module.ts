c2ec72227c8128aa1387a73e16666e4af60d3874
mscamargo <mscamargo@pm.me>
2022-06-03 19:36:20 -0300

    feat: setup TypeORM with Postgres driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'pg',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'vibbra',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
